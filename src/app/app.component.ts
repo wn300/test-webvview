358fe245bc15645b6d0f79e876e987814cc2ec18
wn300 <wilmer.mancera93@gmail.com>
2020-05-30 12:08:37 -0500

    ghp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare global {
+  interface Window {
+    ReactNativeWebView: any;
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-webvview';
+  token = ''
+
+  ngOnInit(): void {
+    this.token = localStorage.getItem('token');
+  }
+
+  injectFromAngularToReactNative(){
+    window.ReactNativeWebView.postMessage('Esto es desde la Angular');
+  }
 }
